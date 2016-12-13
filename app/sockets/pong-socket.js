0160343cec8a36769b0520fa1ffb6d923997d03f
Bryan Schrock <schrock.bryan@heb.com>
2016-12-13 12:56:35 -0600

    pong socket is template

--- app/sockets/pong-socket.js
+++ app/sockets/pong-socket.js
@@ -1,6 +1,23 @@
 function PongSocket(){
-    this.name = "Pong"
+    this.callbackFunction = null;
 }
+
+/*----------Begin general game methods--------------*/
+
+PongSocket.prototype.init = function(userOne,userTwo){
+
+};
+
+PongSocket.prototype.begin = function(){
+
+};
+
+PongSocket.prototype.setFinishCallback = function(callbackFunction){
+    this.callbackFunction = callbackFunction;
+};
+
+/*----------End general game methods--------------*/
+
 
 function getConstructor(){
     return PongSocket;
